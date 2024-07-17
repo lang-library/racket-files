36bb711450b3cb6f67f4ecf48a01092efbffc9d5
JavaCommons Technologies <javacommons@gmail.com>
2024-07-17 19:07:30 +0900

    Files-v2024.0717.1907.29

--- main.rkt
+++ main.rkt
@@ -63,77 +63,6 @@
   (void)
   )
 
-#;(define (to-meta-pair name data)
-  (hasheq '! name '? data)
-  )
-
-#;(define (from-meta-pair ht)
-  (with-handlers ([exn:fail?
-                   ht])
-    (let ([key (hash-ref ht '!)])
-      (cond
-        ((equal? key "bytes") (from-base64 (hash-ref ht '?)))
-        ((equal? key "racket")
-         (define sp (open-input-string (hash-ref ht '?)))
-         (read sp))
-        (#t ht)
-        )
-      )
-    )
-  )
-
-#;(define (to-meta-object x)
-  (define mo
-    (cond
-      ;;((null? x) x)
-      ;;((number? x) x)
-      ;;((string? x) x)
-      ((bytes? x) (to-meta-pair "bytes" (to-base64 x)))
-      ((cons? x) (cons (to-meta-object (car x)) (to-meta-object (cdr x))))
-      ((hash? x)
-       (hash-map/copy
-        x
-        (lambda (k v)
-          (values k (to-meta-object v))
-          )))
-      ((vector? x) (vector->list (vector-map to-meta-object x)))
-      ;;((vector? x) (vector-map to-meta-object x))
-      (#t x)
-      )
-    )
-  (if (jsexpr? mo) mo (to-meta-pair "racket" (print->string mo)))
-  )
-
-#;(define (from-meta-object mo)
-  (define x
-    (cond
-      ;;((null? x) x)
-      ;;((number? x) x)
-      ;;((string? x) x)
-      ;;((bytes? x) (to-meta-pair "bytes" (to-base64 x)))
-      ((cons? mo) (cons (from-meta-object (car mo)) (from-meta-object (cdr mo))))
-      ((hash? mo)
-       (if (hash-has-key? mo '!)
-           (from-meta-pair mo)
-           (hash-map/copy
-            mo
-            (lambda (k v)
-              (values k (from-meta-object v))
-              ))))
-      ((vector? mo) (vector-map from-meta-object mo))
-      (#t mo)
-      )
-    )
-  x
-  )
-
-#;(define (to-json x #:indent? [indent? #f])
-  (define mo (to-meta-object x))
-  (jsexpr->string mo #:indent (if indent? 2 #f))
-  )
-
-#;(define (from-json json) (string->jsexpr json))
-
 (define (files-read-json path)
   (define json (files-read-text path))
   (from-json json)
@@ -142,14 +71,6 @@
 (define (files-write-json path x)
   (define json (to-json x #:indent? #t))
   (files-write-text path json)
-  )
-
-#;(define (to-base64 x)
-  (bytes->string/latin-1 (base64-encode x))
-  )
-
-#;(define (from-base64 x)
-  (base64-decode x)
   )
 
 (provide
